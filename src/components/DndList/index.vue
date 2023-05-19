76d297fecf4d1663d68c86a0d03f650f4162046d
BSY <@bin1728599685>
2023-05-19 18:05:17 +0800

    11

--- src/components/DndList/index.vue
+++ src/components/DndList/index.vue
@@ -11,7 +11,6 @@
             <span style="float: right ;margin-top: -20px;margin-right:5px;" @click="deleteEle(element)">
               <i style="color:#ff4949" class="el-icon-delete" />
             </span>
-            
           </div>
         </div>
       </draggable>
